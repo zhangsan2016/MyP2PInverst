dd56754b01e05ffdfb0d01f85035c4342cd8abac
liu <602694077@qq.com>
2018-09-17 17:50:07 +0800

    2018年9月17日 17:49:55  初始化

---
+++ app/src/main/java/myp2pinverst/ldgd/com/myp2pinverst/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package myp2pinverst.ldgd.com.myp2pinverst.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import myp2pinverst.ldgd.com.myp2pinverst.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
